7d18ecb2df15e882d8ca5c51f7603eee65dda523
jarvychavez <jav.chavez94@gmail.com>
2025-05-26 13:00:05 -0600

    lista del top 10

--- src/main/java/com/aluracursos/desafio/principal/Principal.java
+++ src/main/java/com/aluracursos/desafio/principal/Principal.java
@@ -1,8 +1,11 @@
 package com.aluracursos.desafio.principal;
 
 import com.aluracursos.desafio.model.Datos;
+import com.aluracursos.desafio.model.DatosLibros;
 import com.aluracursos.desafio.service.ConsumoAPI;
 import com.aluracursos.desafio.service.ConvierteDatos;
+
+import java.util.Comparator;
 
 public class Principal {
 
@@ -16,6 +19,14 @@ public class Principal {
         System.out.println(json);
         var datos = conversor.obtenerDatos(json,Datos.class);
         System.out.println(datos);
+
+        //Top 10 de los libros más descargados
+        System.out.println("Top 10 de los libros más descargados ");
+        datos.resultados().stream()
+                .sorted(Comparator.comparing(DatosLibros::numeroDeDescargas).reversed())
+                .limit(10)
+                .map(l->l.titulo().toUpperCase())
+                .forEach(System.out::println);
 
 
     }
